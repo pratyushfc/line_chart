d3f5c7b7397f0262189f3b4576ed6dd327fa0c8f
Pratyush <pratyush.creed@gmail.com>
2016-07-07 13:53:20 +0530

    Render Engine initiated

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,16 +1,12 @@
 // Semicolon; IIFE wont be parsed as function parameter
 ;(function(){
 	"use strict";
-	
-	var global = Function('return this')() || (eval)('this');
 
 	// Exposing public Api
-	global.RenderChart = function(data, selector){
+	window.RenderChart = function(data, selector){
 		var chart = new Chart(data);
-		/*console.log("Min", chart.getMinX(), chart.getMaxX());
-		console.log("Sale", chart.getMinY('sale'), chart.getMaxY('sale'));
-		console.log("Get X", chart.getX());
-		console.log("Get sale", chart.getY("sale"));*/
+		var engine = new Engine(chart);
+		engine.render();
 	};
 
 	var sortByTime = function(a, b){		// Helper function to sort array by time
@@ -260,7 +256,7 @@
 
 		// Step up; Multiplying the value to min-max that was divided before
 
-		stepsDown *= Math.pow(10, stepsDown);
+		steps *= Math.pow(10, stepsDown);
 		computedMin *= Math.pow(10, stepsDown);
 		computedMax *= Math.pow(10, stepsDown);
 
@@ -268,10 +264,111 @@
 
 		while(temp <= computedMax){
 			rangeArray.push(temp);
-			temp += stepsDown;
+			temp += steps;
 		}
 
 		return rangeArray;
 	} // End getYRange
+
+	Engine.prototype.getXRange = function(){
+		var minValue = joinDate(this.chart.getMinX().year, this.chart.getMinX().month);
+		var maxValue = joinDate(this.chart.getMaxX().year, this.chart.getMaxX().month);
+
+		var steps = Math.floor((maxValue - minValue) / 5) ;
+		var rangeArray = [];
+
+		while(minValue <= maxValue){
+			rangeArray.push(minValue);
+			minValue += steps;
+		}
+		return rangeArray;
+
+	} // end getXrange
+
+	Engine.prototype.render = function(){
+		var render = new RenderEngine('graph');
+		render.drawYAxis(this.getYRange("sale"));
+		console.log(this.getYRange("sale"));
+		render.drawXAxis(this.getXRange());
+	}
+
+	function RenderEngine(selector, width, height){
+		width = width ? width : 600;
+		height = height ? height : 500;
+		this.rootElement = document.getElementById(selector);				 // getting parent element
+		this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");	// creating canvas
+		this.svg.setAttribute("height", height);	
+		this.svg.setAttribute("width", width);
+		this.rootElement.appendChild(this.svg);	// adding our canvas to parent element
+		this.marginX = 25;						// Margin will be used for labels
+		this.marginY = 25;						// and ticks
+		this.width = width;						// Storing height and width
+		this.height = height;					// for future uses
+	}
+
+	RenderEngine.prototype.__drawLine = function(x1, y1, x2, y2, style){	// Private function to 
+																	// draw lines
+		var coord1 = this.convert(x1, y1);			// Getting converted axis
+		var coord2 = this.convert(x2, y2);			// according to canvas
+		var lineStyle = style ? style : "stroke:rgb(255,0,0);stroke-width:1";
+		var line = document.createElementNS("http://www.w3.org/2000/svg", "line");	// creating our 
+																					// element line
+		line.setAttribute("x1", coord1.x);			// setting line coordinates
+		line.setAttribute("y1", coord1.y);			// and styles
+		line.setAttribute("x2", coord2.x);
+		line.setAttribute("y2", coord2.y);
+		line.setAttribute("style", lineStyle);
+		this.svg.appendChild(line);					// Drawing line to our canvas
+	} // end constructor function
+
+	RenderEngine.prototype.convert = function (x, y){
+		return {
+			x : x + this.marginX,
+			y : this.height - (y + this.marginY)
+		};
+	} // end convert
+
+	RenderEngine.prototype.drawXAxis = function(rangeArray){
+		var i, len, item;					// Loop iteration variables
+		// Drawing X axis 
+		var x1 = -1 * this.marginX;
+		var x2 = this.width;
+		var y1 = 0;
+		var y2 = 0;
+ 		this.__drawLine(x1, y1, x2, y2);
+
+ 		// Drawing the ticks
+ 		var firstItem = rangeArray[0]; 
+ 		var lastItem = rangeArray[rangeArray.length - 1]; 
+ 		for(i = 0, len = rangeArray.length; i < len; ++i){
+ 			item = rangeArray[i];
+			x1 = ((item - firstItem) / (lastItem - firstItem)) * this.width * 0.9;
+			x2 = ((item - firstItem) / (lastItem - firstItem)) * this.width * 0.91;
+			y1 = -4;
+			y2 = 4;
+	 		this.__drawLine(x1, y1, x2, y2); 			
+ 		}
+	} // end draw x axis
+
+	RenderEngine.prototype.drawYAxis = function(rangeArray){
+		var i, len, item;
+		var x1 = 0;
+		var x2 = 0;
+		var y1 = -1 * this.marginX;
+		var y2 = this.height;
+ 		this.__drawLine(x1, y1, x2, y2);
+ 		// Drawing the ticks
+ 		var firstItem = rangeArray[0]; 
+ 		var lastItem = rangeArray[rangeArray.length - 1]; 
+ 		for(i = 0, len = rangeArray.length; i < len; ++i){
+ 			item = rangeArray[i];
+			y1 = ((item - firstItem) / (lastItem - firstItem)) * this.height * 0.9;
+			y2 = ((item - firstItem) / (lastItem - firstItem)) * this.height * 0.91;
+			x1 = -4;
+			x2 = 4;
+	 		this.__drawLine(x1, y1, x2, y2); 			
+ 		}
+	} // end drawYAxis
+
 
 })();
